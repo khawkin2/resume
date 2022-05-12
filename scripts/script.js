f0b8de99583888f8d461c1df8cdf7bdec9583c4c
Kayla <khawkin2@unb.ca>
2022-05-12 17:14:47 -0300

    added button to randomly change background colour

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,13 @@
 function loadPage(location) {
     window.location = location;
 }
+
+function changeBackgroundColour() {
+    let colour = randomColour();
+    console.log("Random colour Generated: " + colour);
+    document.body.style.backgroundColor = colour;
+}
+
+function randomColour(){
+    return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+}
